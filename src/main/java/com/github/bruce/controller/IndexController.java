69f0288946eb654f8b668147e8f5e2f729944bd8
yandajun <yandajun@vipkid.com.cn>
2018-02-05 11:06:11 +0800

    test post and get method

--- src/main/java/com/github/bruce/controller/IndexController.java
+++ src/main/java/com/github/bruce/controller/IndexController.java
@@ -1,7 +1,11 @@
 package com.github.bruce.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * Description
@@ -17,5 +21,18 @@ public class IndexController {
     @RequestMapping("/")
     public String welcome(){
         return "Welcome to my-spring-boot !";
+    }
+
+    /**
+     * receive get param 'a', post json param b, for example:
+     * /test/post?a=123
+     * post json b: {"x":1, "y":2}
+     * @param a
+     * @param b
+     * @return
+     */
+    @RequestMapping(value = "/test/post", method = RequestMethod.POST)
+    public String testPost(String a, @RequestBody Map<String, String> b) {
+        return a + b;
     }
 }
