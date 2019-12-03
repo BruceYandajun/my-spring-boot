0a0c0b0fe7cc2e274988c778ba8b4a45f686b50d
yandajun <yandajun@vipkid.com.cn>
2019-12-03 17:02:26 +0800

    CopyOnWriteArrayList

--- src/main/java/com/github/bruce/reactive/flux/FluxSchedulerTest.java
+++ src/main/java/com/github/bruce/reactive/flux/FluxSchedulerTest.java
@@ -9,34 +9,67 @@ import reactor.core.scheduler.Schedulers;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.github.bruce.utils.BaseUtil.line;
 
 @Slf4j
 public class FluxSchedulerTest {
     public static void main(String[] args) {
-        StopWatch watch = new StopWatch();
-        watch.start("flux");
-        List<Integer> list = new ArrayList<>();
-        Flux.just("abc", "hello", "ddd")
-                .flatMap(key -> get(key, list))
-                .collectList().block();
-        watch.stop();
-        log.info(watch.prettyPrint());
-        line(list);
+        CountDownLatch runningCounter = new CountDownLatch(1);
+        List<Thread> workers = Stream.generate(() -> new Thread(new Worker(runningCounter))).limit(10).collect(Collectors.toList());
+        workers.forEach(Thread::start);
+        runningCounter.countDown();
+
     }
 
-    private static Mono<List<Integer>> get(String key, List<Integer> list) {
+    public static void test() {
+//        StopWatch watch = new StopWatch("Watch");
+//        watch.start("flux");
+        List<Integer> list = new CopyOnWriteArrayList<>();
+        List<Integer> block = Flux.just("abc", "hello", "ddd")
+                .flatMap(key -> get(key, list)).collectList().block();
+//        watch.stop();
+//        System.out.println(watch.prettyPrint());
+        line(list);
+        line(block);
+//        System.out.println("=================");
+//        System.out.println("=================");
+    }
+
+    private static Mono<Integer> get(String key, List<Integer> list) {
         return Mono.fromCallable(() -> {
             try {
                 Thread.sleep(2000);
-                System.out.println("get key: " + key + ", in thread " + Thread.currentThread().getName());
+//                System.out.println("get key: " + key + ", in thread " + Thread.currentThread().getName());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            List<Integer> integers = Collections.singletonList(key.hashCode());
-            list.addAll(integers);
-            return integers;
+            list.add(key.hashCode());
+            return key.hashCode();
         }).subscribeOn(Schedulers.elastic());
+    }
+
+}
+
+class Worker implements Runnable {
+
+    private CountDownLatch runningCounter;
+
+    Worker(CountDownLatch counter) {
+        runningCounter = counter;
+    }
+
+    @Override
+    public void run() {
+        try {
+            runningCounter.await();
+            FluxSchedulerTest.test();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
