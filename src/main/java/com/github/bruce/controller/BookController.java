9fd948998eb7cc8472e170f3612cca508a9a3112
yandajun <yandajun@vipkid.com.cn>
2019-12-26 01:33:05 +0800

    Resource

--- src/main/java/com/github/bruce/controller/BookController.java
+++ src/main/java/com/github/bruce/controller/BookController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/book")
 public class BookController {
 
-    @Autowired
+    @Resource
     private UserService userService;
 
     @GetMapping("/getById")
