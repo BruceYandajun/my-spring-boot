b882f03b90d8e77c4c0a64fcb60dd696d3f69fe1
yandajun <yandajun@vipkid.com.cn>
2020-02-04 10:17:01 +0800

    Convert list

--- src/main/java/com/github/bruce/model/converter/ParentConverter.java
+++ src/main/java/com/github/bruce/model/converter/ParentConverter.java
@@ -4,11 +4,15 @@ import com.github.bruce.model.Parent;
 import com.github.bruce.model.dto.ParentDto;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
 
 @Mapper(componentModel = "spring", uses = DateConverter.class, nullValueCheckStrategy = ALWAYS)
 public interface ParentConverter {
 
     ParentDto convert(Parent parent);
+
+    List<ParentDto> convert(List<Parent> parents);
 
 }
