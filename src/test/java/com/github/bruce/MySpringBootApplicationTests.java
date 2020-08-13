bce24224c19a3985d5a515b28696deed2d46aabc
yandajun <yandajun@vipkid.com.cn>
2020-08-13 17:41:48 +0800

    config

--- src/test/java/com/github/bruce/MySpringBootApplicationTests.java
+++ src/test/java/com/github/bruce/MySpringBootApplicationTests.java
@@ -2,6 +2,7 @@ package com.github.bruce;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,9 +10,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MySpringBootApplicationTests {
 
+	@Value("${config.test}")
+	private String configTest;
+
 	@Test
 	public void contextLoads() {
 		System.out.println("spring boot test");
+		System.out.println(configTest);
 	}
 
 }
