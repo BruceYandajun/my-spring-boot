8e64afc244503628e5520c9e86a6392d44388b9e
Bruce <ydjidil@163.com>
2021-05-18 10:22:47 +0800

    test

--- src/test/java/com/github/bruce/StringFormatTest.java
+++ src/test/java/com/github/bruce/StringFormatTest.java
@@ -1,2 +1,8 @@
-package com.github.bruce;public class StringFormatTest {
+package com.github.bruce;
+public class StringFormatTest {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(String.format("%d", System.currentTimeMillis()));
+    }
 }
+
