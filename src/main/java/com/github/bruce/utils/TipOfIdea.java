85dd0ec539228bb8fbe14850cacac0c5d649b209
Bruce <ydjidil@163.com>
2021-07-30 10:23:41 +0800

    tests of tips

---
+++ src/main/java/com/github/bruce/utils/TipOfIdea.java
@@ -0,0 +1,21 @@
+package com.github.bruce.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TipOfIdea {
+    public static void main(String[] args) {
+        int abcd = 0;
+        int b = 0;
+
+    }
+
+    private int test() {
+        List<String> l = new ArrayList<>();
+        String[] p = l.toArray(new String[0]);
+
+        return 1;
+    }
+}
